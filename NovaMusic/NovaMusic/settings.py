94265d717c6b643839bb8c3b574dd7cd14b16204
DTC174 <a01672034597@gmail.com>
2025-05-22 10:42:14 +0700

    Fix Update

--- NovaMusic/NovaMusic/settings.py
+++ NovaMusic/NovaMusic/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mq1nim^d+7#9n%mdbiu*6wvr4*g=#2gib=!t)@3(!5b6u9x-$='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
